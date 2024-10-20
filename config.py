bccdeaaded2d1d30576185da52ea6d9311333f90
RudranshSingh05 <126321741+RudranshSingh05@users.noreply.github.com>
2024-10-20 14:31:57 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+import time
+import sys
+from langchain.callbacks.base import BaseCallbackHandler
+
+PINECONE_API_KEY = "077cedce-098f-420e-a7bc-3abe324476b5"
+os.environ["GROQ_API_KEY"] = "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"
+
+class TypewriterCallbackHandler(BaseCallbackHandler):
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        sys.stdout.write(token)
+        sys.stdout.flush()
+        time.sleep(0.02)
